caa0bed1f1d959452f1225ebdc1b0521a3378c3f
iAamAhmad <aghazi1011@gmail.com>
2024-03-16 13:39:02 +0500

    form-fields

---
+++ react-comonents-used/src/components/CssPractice/Positioning.jsx
@@ -0,0 +1,16 @@
+
+import "./Positioning.css"
+const PracticeCss = () => {
+   return (
+      <div className="container-parent">
+
+         <div className="topleft">Top Left</div>
+         <div className="topright">Top Right</div>
+         <div className="bottomright">bottom Right</div>
+         <div className="bottomleft">Bottom Left</div>
+      </div>
+
+   )
+}
+
+export default PracticeCss
