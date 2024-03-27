bad09d539250669ec4e5fcb33a63aed51d9160f2
iAamAhmad <aghazi1011@gmail.com>
2024-03-27 06:01:33 +0500

    second

--- react-comonents-used/src/pages/FShapedLayout.jsx
+++ react-comonents-used/src/pages/FShapedLayout.jsx
@@ -21,6 +21,24 @@ const FShapedLayout = () => {
         <div className="min-h-[300px] bg-slate-500 sm:col-span-3 hidden sm:block">Display only Lg</div>
 
       </div>
+      <div className="grid sm:grid-cols-12 gap-3 m-2 text-center">
+        <div className="min-h-[300px] bg-red-300 sm:col-span-3 hidden sm:block">Display only Lg</div>
+        <div className="min-h-[300px] bg-green-500 sm:col-span-6">Main Content</div>
+        <div className="min-h-[300px] bg-slate-500 sm:col-span-3 hidden sm:block">Display only Lg</div>
+
+      </div>
+      <div className="grid sm:grid-cols-12 gap-3 m-2 text-center">
+        <div className="min-h-[300px] bg-red-300 sm:col-span-3 hidden sm:block">Display only Lg</div>
+        <div className="min-h-[300px] bg-green-500 sm:col-span-6">Main Content</div>
+        <div className="min-h-[300px] bg-slate-500 sm:col-span-3 hidden sm:block">Display only Lg</div>
+
+      </div>
+      <div className="grid sm:grid-cols-12 gap-3 m-2 text-center">
+        <div className="min-h-[300px] bg-red-300 sm:col-span-3 hidden sm:block">Display only Lg</div>
+        <div className="min-h-[300px] bg-green-500 sm:col-span-6">Main Content</div>
+        <div className="min-h-[300px] bg-slate-500 sm:col-span-3 hidden sm:block">Display only Lg</div>
+
+      </div>
     </div>
   )
 }
