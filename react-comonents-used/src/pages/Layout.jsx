732d27bbc83e668f58e5107d546a5e32b72e49b7
iAamAhmad <aghazi1011@gmail.com>
2024-03-30 05:46:41 +0500

    maap

--- react-comonents-used/src/pages/Layout.jsx
+++ react-comonents-used/src/pages/Layout.jsx
@@ -3,11 +3,15 @@
 const Layout = () => {
    return (
       <div>
-         datas.map((data,index)=>{
-            <p>1</p>
-         })
-         
-         
+         datas.map((data,index) => (<p>1</p>))
+
+         console.log(score);  // 0
+         setScore(score + 1); // setScore(0 + 1);
+         console.log(score);  // 0
+         setScore(score + 1); // setScore(0 + 1);
+         console.log(score);  // 0
+         setScore(score + 1); // setScore(0 + 1);
+         console.log(score);  // 0
       </div>
    )
 }
