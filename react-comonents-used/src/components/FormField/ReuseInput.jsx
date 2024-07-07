3223e93e1a24ed4e1f571f7b957d25c868b5d022
iAamAhmad <ahmad441026@gmail.com>
2024-07-07 18:00:02 +0500

    re useable react components

--- react-comonents-used/src/components/FormField/ReuseInput.jsx
+++ react-comonents-used/src/components/FormField/ReuseInput.jsx
@@ -1,12 +1,33 @@
+/* eslint-disable react/prop-types */
 
 
-const ReuseInput = () => {
+const InputField = ({
+  label,
+  type = 'text',
+  name,
+  value,
+  onChange,
+  register,
+  requiredMessage,
+  error,
+}) => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-1 mb-4">
+      <label htmlFor={name} className="font-bold">
+        {label}
+      </label>
+      <input
+        className="border p-2"
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        {...register(name, { required: requiredMessage })}
+      />
+      {error && <span className="text-blue-600">{error.message}</span>}
     </div>
-  )
-}
+  );
+};
 
-export default ReuseInput
-
+export default InputField;
--- react-comonents-used/src/pages/ReuseAbleForm.jsx
+++ react-comonents-used/src/pages/ReuseAbleForm.jsx
@@ -1,11 +1,136 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import InputField from '../components/FormField/ReuseInput';
 
 
-const ReuseAbleForm = () => {
+const FormFieldCustom = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [formData, setFormData] = useState({
+    userName: '',
+    email: '',
+    profession: '',
+    gender: '',
+    languages: [],
+  });
+
+  const onChangeHandler = (e) => {
+    const { name, value, type, checked } = e.target;
+    if (type === 'checkbox') {
+      setFormData((prevData) => {
+        const updateLang = checked
+          ? [...prevData.languages, value]
+          : prevData.languages.filter((lang) => lang !== value);
+
+        return {
+          ...prevData,
+          languages: updateLang,
+        };
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+  const onSubmithandler = (data) => {
+    console.log('FormData : ', data);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h1 className="font-bold">A Custom Field</h1>
+      <form className="m-2" onSubmit={handleSubmit(onSubmithandler)}>
+        <InputField
+          label="UserName"
+          name="userName"
+          value={formData.userName}
+          onChange={onChangeHandler}
+          register={register}
+          requiredMessage="Name is required"
+          error={errors.userName}
+        />
+        <InputField
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={onChangeHandler}
+          register={register}
+          requiredMessage="Email is required"
+          error={errors.email}
+        />
+        <div>
+          <label className="font-bold">Gender</label>
+          <div>
+            <InputField
+              label="Male"
+              type="radio"
+              name="gender"
+              value="Male"
+              onChange={onChangeHandler}
+              checked={formData.gender === 'Male'}
+              register={register}
+              requiredMessage="Gender is required"
+              error={errors.gender}
+            />
+            <InputField
+              label="Female"
+              type="radio"
+              name="gender"
+              value="Female"
+              onChange={onChangeHandler}
+              checked={formData.gender === 'Female'}
+              register={register}
+              requiredMessage="Gender is required"
+              error={errors.gender}
+            />
+          </div>
+        </div>
+        <div>
+          <label className="font-bold">Languages</label>
+          <div className="flex gap-2">
+            <InputField
+              label="Urdu"
+              type="checkbox"
+              name="languages"
+              value="Urdu"
+              onChange={onChangeHandler}
+              checked={formData.languages.includes('Urdu')}
+              register={register}
+              requiredMessage="Languages are required"
+              error={errors.languages}
+            />
+            <InputField
+              label="English"
+              type="checkbox"
+              name="languages"
+              value="English"
+              onChange={onChangeHandler}
+              checked={formData.languages.includes('English')}
+              register={register}
+              requiredMessage="Languages are required"
+              error={errors.languages}
+            />
+            <InputField
+              label="German"
+              type="checkbox"
+              name="languages"
+              value="German"
+              onChange={onChangeHandler}
+              checked={formData.languages.includes('German')}
+              register={register}
+              requiredMessage="Languages are required"
+              error={errors.languages}
+            />
+          </div>
+        </div>
+        <button type="submit" className="mt-4 p-2 bg-blue-500 text-white">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
 
-export default ReuseAbleForm
+export default FormFieldCustom;
